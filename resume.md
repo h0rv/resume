e83858000151392d4afccf82b13902964c9d0901
Robby <h0rv@users.noreply.github.com>
2024-04-13 13:06:46 -0400

    Update

--- resume.md
+++ resume.md
@@ -18,30 +18,28 @@ is intended to be used for a short summary. -->
 <!-- You have to wrap the "left" and "right" half of these headings in spans by
 hand -->
 
-### <span>Software Engineer - IBM Software</span> <span>June 2023 -- Present</span>
+### <span>Software Engineer - IBM Software - MultiCloud Platforms</span> <span>January 2024 -- Present</span>
 
   Raleigh-Durham, NC
 
-  - Developing infrastructure to expand and automate a Multicloud platform to include Azure; using Terraform, Ansible, and ArgoCD.
-  - Researching and developing LLM-powered applications (chatbots and agents) using LangChain and watsonx.
+  - Significantly contributing to the expansion of the MultiCloud platform to include Azure, automating the infrastructure using a GitOps workflow with Terraform/Tofu, Atlantis, Go, and ArgoCD.
+  - Leading the research and development of a RAG (Retrieval Augmented Generative) LLM chatbot to enhance customer onboarding and documentation Q&A.
 
-### <span>Software Engineer - IBM Cloud (Continuous Delivery (CD))</span> <span>June 2023 -- Present</span>
+### <span>Software Engineer - IBM Cloud Continuous Delivery (CD)</span> <span>June 2023 -- January 2024</span>
 
   Raleigh-Durham, NC
 
-  - Led critical feature development for our Node.js service, ensuring end-to-end implementation and successful production deployment.
-  - Established comprehensive test coverage using Jest for our service, integrated into our CI pipeline for validation on every pull commit in a pull request.
-  - Improved maintainability and simplified debugging through significant codebase refactoring.
-  - Collaborated closely with end-users to troubleshoot and resolve challenges related to our services.
-  - Contributed to infrastructure with Kubernetes (Helm, Tekton), containers (Podman, Docker), Terraform, and observability tools.
-  - Enhanced an internal CLI tool for live debugging of production environments and cluster management, packaging it with pip for a simple installation process for others.
+  - Led critical feature development for Node.js service, ensuring on-time production roll-out.
+  - Established comprehensive test coverage using Jest, integrating it into the CI (Continuous Integration) pipeline to maintain service reliability.
+  - Significantly refactored the codebase, greatly improving maintainability, performance, and the overall debugging experience.
+  - Provided support to end-users to troubleshoot and resolve issues related to our services.
 
 ### <span>Software Engineer Intern - IBM Cloud (Continuous Delivery (CD))</span> <span>May 2022 -- Dec 2022</span>
 
   Raleigh-Durham, NC (Remote)
 
-  - Delivered a new feature to production with proper tests within the initial 3 weeks, using Golang despite being new to the language.
-  - Improved observability by optimizing performance monitoring for our team's Node microservices and automated the deployment of observability setups across multiple cloud regions.
+  - Delivered a new feature to production with proper tests within the initial 3 weeks in Go despite being completely new to the language.
+  - Improved observability by optimizing performance monitoring for all Node microservices and automated the deployment of observability setups across multiple cloud regions.
 
 ## Education
 
@@ -53,14 +51,12 @@ hand -->
 
 ### <span>mjc (MiniJava Compiler)</span> <span>_Code available upon request_</span>
 
-  - Developed a compiler that supports a subset of Java (MiniJava) as the source language with MIPS assembly as the target language.
+  - Developed MiniJava (subset of Java) compiler that with MIPS assembly as the target language.
   - Created using Java, JFlex (lexical analysis), and JavaCUP (parsing).
 
 ### <span>Deep Answers</span> <span>[_github.com/h0rv/deep-answers_](https://github.com/h0rv/deep-answers)</span>
 
-  - Developed a (Large language model) LLM-based chatbot with retrieval augmented generation (RAG) to analyze podcast transcripts from a favorite podcast.
-  - Provides a platform for generating answers and responses to questions and prompts related to the podcast content, promoting the dissemination of ideas, advice, and information within the podcast's domain.
-  - Uses the Llama 2 model, ChromaDB vector database, and streamlit for the frontend.
+  - Created an LLM-based chatbot with RAG to analyze and "chat" with a podcast from its transcriptions.
 
 ### <span>TCP Implementation</span> <span>_Code available upon request_</span>
 
@@ -68,6 +64,6 @@ hand -->
 
 ## Skills
 
-  - __Languages:__ JavaScript (Node.js), Go, Python, C, Java, Rust, Haskell, Shell (Bash, Fish)
-  - __Technologies:__ GNU/Linux, Redis, Containers, Kubernetes, Git, NeoVim
+  - __Languages:__ JavaScript (Node.js), Go, Python, Terraform/Tofu, C, Java, Rust, Haskell, Shell (Bash, Fish)
+  - __Technologies:__ GNU/Linux, Redis, Podman/Docker, Kubernetes, Git, NeoVim
 
--- resume.py
+++ resume.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 import argparse
-import base64
-import itertools
 import logging
 import os
-import re
-import shutil
-import subprocess
-import sys
-import tempfile
 
 import markdown
+from weasyprint import HTML
 
 preamble = """\
 <html lang="en">
@@ -31,58 +25,6 @@ postamble = """\
 </html>
 """
 
-CHROME_GUESSES_MACOS = (
-    "/Applications/Chromium.app/Contents/MacOS/Chromium",
-    "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary",
-    "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-)
-
-# https://stackoverflow.com/a/40674915/409879
-CHROME_GUESSES_WINDOWS = (
-    # Windows 10
-    os.path.expandvars(r"%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"),
-    os.path.expandvars(r"%ProgramFiles%\Google\Chrome\Application\chrome.exe"),
-    os.path.expandvars(r"%LocalAppData%\Google\Chrome\Application\chrome.exe"),
-    # Windows 7
-    r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
-    r"C:\Program Files\Google\Chrome\Application\chrome.exe",
-    # Vista
-    r"C:\Users\UserName\AppDataLocal\Google\Chrome",
-    # XP
-    r"C:\Documents and Settings\UserName\Local Settings\Application Data\Google\Chrome",
-)
-
-# https://unix.stackexchange.com/a/439956/20079
-CHROME_GUESSES_LINUX = [
-    "/".join((path, executable))
-    for path, executable in itertools.product(
-        (
-            "/usr/local/sbin",
-            "/usr/local/bin",
-            "/usr/sbin",
-            "/usr/bin",
-            "/sbin",
-            "/bin",
-            "/opt/google/chrome",
-        ),
-        ("google-chrome", "chrome", "chromium", "chromium-browser"),
-    )
-]
-
-
-def guess_chrome_path() -> str:
-    if sys.platform == "darwin":
-        guesses = CHROME_GUESSES_MACOS
-    elif sys.platform == "win32":
-        guesses = CHROME_GUESSES_WINDOWS
-    else:
-        guesses = CHROME_GUESSES_LINUX
-    for guess in guesses:
-        if os.path.exists(guess):
-            logging.info("Found Chrome or Chromium at " + guess)
-            return guess
-    raise ValueError("Could not find Chrome. Please set CHROME_PATH.")
-
 
 def title(md: str) -> str:
     """
@@ -90,7 +32,7 @@ def title(md: str) -> str:
     assume to be the title of the document.
     """
     for line in md.splitlines():
-        if re.match("^#[^#]", line):  # starts with exactly one '#'
+        if line.startswith("#"):
             return line.lstrip("#").strip()
     raise ValueError(
         "Cannot find any lines that look like markdown h1 headings to use as the title"
@@ -118,57 +60,12 @@ def make_html(md: str, prefix: str = "resume") -> str:
     )
 
 
-def write_pdf(html: str, prefix: str = "resume", chrome: str = "") -> None:
+def write_pdf(html: str, prefix: str = "resume") -> None:
     """
-    Write html to prefix.pdf
+    Write html to prefix.pdf using weasyprint
     """
-    chrome = chrome or guess_chrome_path()
-    html64 = base64.b64encode(html.encode("utf-8"))
-    options = [
-        "--no-sandbox",
-        "--headless",
-        "--print-to-pdf-no-header",
-        "--enable-logging=stderr",
-        "--log-level=2",
-        "--in-process-gpu",
-        "--disable-gpu",
-    ]
-
-    # Ideally we'd use tempfile.TemporaryDirectory here. We can't because
-    # attempts to delete the tmpdir fail on Windows because Chrome creates a
-    # file the python process does not have permission to delete. See
-    # https://github.com/puppeteer/puppeteer/issues/2778,
-    # https://github.com/puppeteer/puppeteer/issues/298, and
-    # https://bugs.python.org/issue26660. If we ever drop Python 3.9 support we
-    # can use TemporaryDirectory with ignore_cleanup_errors=True as a context
-    # manager.
-    tmpdir = tempfile.mkdtemp(prefix="resume.md_")
-    options.append(f"--crash-dumps-dir={tmpdir}")
-    options.append(f"--user-data-dir={tmpdir}")
-
-    try:
-        subprocess.run(
-            [
-                chrome,
-                *options,
-                f"--print-to-pdf={prefix}.pdf",
-                "data:text/html;base64," + html64.decode("utf-8"),
-            ],
-            check=True,
-        )
-        logging.info(f"Wrote {prefix}.pdf")
-    except subprocess.CalledProcessError as exc:
-        if exc.returncode == -6:
-            logging.warning(
-                "Chrome died with <Signals.SIGABRT: 6> "
-                f"but you may find {prefix}.pdf was created successfully."
-            )
-        else:
-            raise exc
-    finally:
-        shutil.rmtree(tmpdir, ignore_errors=True)
-        if os.path.isdir(tmpdir):
-            logging.debug(f"Could not delete {tmpdir}")
+    HTML(string=html).write_pdf(f"{prefix}.pdf")
+    logging.info(f"Wrote {prefix}.pdf")
 
 
 if __name__ == "__main__":
@@ -188,10 +85,6 @@ if __name__ == "__main__":
         "--no-pdf",
         help="Do not write pdf output",
         action="store_true",
-    )
-    parser.add_argument(
-        "--chrome-path",
-        help="Path to Chrome or Chromium executable",
     )
     parser.add_argument("-q", "--quiet", action="store_true")
     parser.add_argument("--debug", action="store_true")
@@ -216,4 +109,4 @@ if __name__ == "__main__":
             logging.info(f"Wrote {htmlfp.name}")
 
     if not args.no_pdf:
-        write_pdf(html, prefix=prefix, chrome=args.chrome_path)
+        write_pdf(html, prefix=prefix)
